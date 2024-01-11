09f1fab8b93cb7f22274801ce554c61939611b2a
오준호 <jh.oh@itso.co.kr>
2024-01-11 11:30:07 +0900

    js 파일 수정

--- js/button.js
+++ js/button.js
@@ -1,5 +1,24 @@
-function next1() {
-    if ($('#next-step2').css('display') == 'none') {
-        $('#next-step2').css('display', 'block');
-    }
+
+$(document).ready(function(){
+    $(".menu-box .menu").click(function (){
+        var index = $(this).index();
+
+        //Menu Event
+        $(".menu-box .menu").removeClass("click");
+        $(this).addClass("click");
+
+        //Tap area Event
+        $(".form-box .menu-area").hide();
+        $(".form-box .menu-area").eq(index).show();
+    });
+});
+
+function next(idx){
+    $(".form-box .menu-area").hide();
+    $(".form-box .menu-area").eq(idx + 1).show();
+}
+
+function prev(idx){
+    $(".form-box .menu-area").hide();
+    $(".form-box .menu-area").eq(idx - 1).show();
 }
